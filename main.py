d85bee262a4d67be0f78d80d46ce23b53e8107e3
vijay <sahanivijay800@gmail.com>
2020-12-24 14:23:58 +0530

    comit

--- main.py
+++ main.py
@@ -33,10 +33,10 @@ def valid(bo,num,pos):
             return False
     
     # checking the box
-    box_x=pos[1]//3
-    box_y=pos[0]//3
-    for i in range(box_y*3,box_y*3+3):
-        for j in range(box_x*3,box_x*3+3):
+    box_x=pos[0]//3
+    box_y=pos[1]//3
+    for i in range(box_x*3,box_x*3+3):
+        for j in range(box_y*3,box_y*3+3):
             if bo[i][j]==num and (i,j)!=pos:
                 return False
 
