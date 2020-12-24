73e654fd877928a74f5ea6067443ccd6ec0a00b1
vijay <sahanivijay800@gmail.com>
2020-12-24 22:55:16 +0530

    added more board

--- UI.py
+++ UI.py
@@ -2,7 +2,7 @@ import pygame
 from pygame import mouse
 from pygame.locals import *
 import time
-from main import find_empty, valid ,solve
+from main import find_empty, valid ,solve,board
 
 
 pygame.font.init()
@@ -10,17 +10,7 @@ fnt=pygame.font.SysFont("comiscans",40)
 
 
 class Grid:
-    Board= [
-        [7, 8, 0, 4, 0, 0, 1, 2, 0],
-        [6, 0, 0, 0, 7, 5, 0, 0, 9],
-        [0, 0, 0, 6, 0, 1, 0, 7, 8],
-        [0, 0, 7, 0, 4, 0, 2, 6, 0],
-        [0, 0, 1, 0, 5, 0, 9, 3, 0],
-        [9, 0, 4, 0, 6, 0, 0, 0, 5],
-        [0, 7, 0, 3, 0, 0, 0, 1, 2],
-        [1, 2, 0, 0, 0, 7, 4, 0, 0],
-        [0, 4, 9, 2, 0, 6, 0, 0, 7]
-    ]
+    Board= board
     def __init__(self,rows,cols,width,height,screen):
         self.cubes=[[Cube(self.Board[i][j],i,j,width,height) for j in range(cols)] for i in range(rows)]
         self.rows=rows 
